c464022bf7bb024ed9d1aa686fcdd5284503929e
Adelie <elisabethadelie@gmail.com>
2025-06-08 13:53:02 -0400

    add addSong() function

--- src/Spinlist.cpp
+++ src/Spinlist.cpp
@@ -3,4 +3,19 @@
 #include <algorithm>
 #include <vector>
 
-// ***
+void Spinlist::addSong(std::string title, std::string artist, double duration) {
+    Song newSong(title, artist, duration);  // create a Song object
+    Node* newNode = new Node(newSong);  // wrap it in a new Node
+
+    if (head == nullptr) {
+        // if list is empty, new node is head, tail, and current
+        head = tail = current = newNode;
+    } else {
+        // add new song to end of list
+        tail->next = newNode;  // link the last node's "next" to the new one
+        newNode->prev = tail;  // link new node's "prev" back to the old tail
+        tail = newNode;        // move tail to point to the new node
+    }
+
+    std::cout << "Added: \"" << title << "\" by " << artist << " (" << duration << " mins)" << std::endl;
+}
--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,17 @@
 
 int main() {
     Spinlist myPlaylist;
-    // CLI loop or hardcoded test cases
+    
+    playlist.addSong("In Camera", "Yumi Zouma", 3.24);
+    playlist.addSong("Mulholland", "Hope Tala, sky", 3.44); // handles multiple artists in one string
+    playlist.addSong("Peaceful Place", "Leon Bridges", 4.15);
+    playlist.addSong("Bilgewater", "Brown Bird", 3.55);
+    playlist.addSong("ENTROPY", "Daniel Caesar", 4.21);
+    playlist.addSong("Cosmic", "Amber Mark", 4.35);
+    playlist.addSong("Red Bottom Sky", "Yung Lean", 5.03);
+    playlist.addSong("Hazey", "Glass Animals", 4.26);
+    playlist.addSong("Nothing Better", "The Postal Service", 3.47);
+    playlist.addSong("Le Matin", "Yann Tiersen", 1.59);
+
     return 0;
 }
