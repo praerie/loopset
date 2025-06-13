4d48c113fd86ab21da736215476a56eadd0c0176
Adelie <elisabethadelie@gmail.com>
2025-06-13 00:11:01 -0400

    update preloaded songs

--- src/main.cpp
+++ src/main.cpp
@@ -9,16 +9,16 @@ int main() {
     LoopSet myPlaylist;
 
     // preload songs
-    myPlaylist.addSong("In Camera", "Yumi Zouma", 3, 24);
-    myPlaylist.addSong("Mulholland", "Hope Tala, sky", 3, 44);
-    myPlaylist.addSong("Peaceful Place", "Leon Bridges", 4, 15);
-    myPlaylist.addSong("Bilgewater", "Brown Bird", 3, 55);
     myPlaylist.addSong("ENTROPY", "Daniel Caesar", 4, 21);
     myPlaylist.addSong("Cosmic", "Amber Mark", 4, 35);
     myPlaylist.addSong("Red Bottom Sky", "Yung Lean", 5, 3);
     myPlaylist.addSong("Hazey", "Glass Animals", 4, 26);
     myPlaylist.addSong("Nothing Better", "The Postal Service", 3, 47);
     myPlaylist.addSong("Le Matin", "Yann Tiersen", 1, 59);
+    myPlaylist.addSong("In Dreams", "Roy Orbison", 3, 24);
+    myPlaylist.addSong("Mulholland", "Hope Tala, sky", 3, 44);
+    myPlaylist.addSong("Peaceful Place", "Leon Bridges", 4, 15);
+    myPlaylist.addSong("Bilgewater", "Brown Bird", 3, 55);
 
     int choice;
 
